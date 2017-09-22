bd2037ae207a772e6e6994d3dc0a24f3d9bd5650
Liefu Zhang <liefu.zhang@aderant.com>
2017-09-22 16:50:09 +1200

    update

--- 1.js
+++ 1.js
@@ -14,12 +14,21 @@ var timer = {
         } else {
             this.sec--;
         }
-        $("#display-minute").text(this.min+":"+this.sec);
+        this.updateUI();
     },
     stopTimer: function () {
         if (this.handler) {
             clearInterval(this.handler);
         }
+    },
+    setMin: function (min) {
+        this.min = min;
+        this.sec = 0;
+        this.updateUI(true);
+    },
+    updateUI: function (noSec) {
+        var display = noSec? this.min:this.min + ":" + this.sec;
+        $("#display-minute").text(display);
     }
 }
 
@@ -27,27 +36,38 @@ function setupClickHandler() {
     var $breakNum = $('#break-num');
     var $sessionNum = $('#session-num');
     $('#break-minus').click(() => {
+        if (timer.running) return;
         var val = $breakNum.text();
-        if (val > 1) $breakNum.text(Number(val) - 1);
+        if (val > 1) {
+            $breakNum.text(Number(val) - 1);
+        }
     });
     $('#break-plus').click(() => {
+        if (timer.running) return;
         var val = $breakNum.text();
         $breakNum.text(Number(val) + 1);
     });
     $('#session-minus').click(() => {
+        if (timer.running) return;
         var val = $sessionNum.text();
-        if (val > 1) $sessionNum.text(Number(val) - 1);
+        if (val > 1) {
+            $sessionNum.text(Number(val) - 1);
+            timer.setMin(Number(val) - 1);
+        }
     });
     $('#session-plus').click(() => {
+        if (timer.running) return;
         var val = $sessionNum.text();
         $sessionNum.text(Number(val) + 1);
+        timer.setMin(Number(val) + 1);
     });
 
-    $('#display').click(function() {
+    $('#display').click(function () {
         if (timer.running) {
             timer.stopTimer();
             timer.running = false;
         } else {
+            timer.elapsed();
             timer.handler = setInterval(timer.elapsed.bind(timer), 1000);
             timer.running = true;
         }
